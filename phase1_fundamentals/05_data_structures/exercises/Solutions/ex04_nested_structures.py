e42181a5f21fa0ea54b960b1e9c43a877f4ae04f
Kanishka Harwani <50087038+KanishkaHarwani@users.noreply.github.com>
2026-07-23 10:12:28 +0530

    Update ex04_nested_structures.py

--- phase1_fundamentals/05_data_structures/exercises/Solutions/ex04_nested_structures.py
+++ phase1_fundamentals/05_data_structures/exercises/Solutions/ex04_nested_structures.py
@@ -1,5 +1,17 @@
 def main():
-    pass
+    school_dict = [
+        {"Name": "Kanishka", "Age": 27, "Grade": 98},
+        {"Name": "Hari", "Age": 22, "Grade": 70},
+        {"Name": "Sakshi", "Age": 24, "Grade": 82},
+        {"Name": "Piyush", "Age": 21, "Grade": 75},
+        {"Name": "Kashish", "Age": 23, "Grade": 65},
+        {"Name": "Tanmay", "Age": 21, "Grade": 55},
+        {"Name": "Mayan", "Age": 21, "Grade": 82},
+        {"Name": "Pankaj", "Age": 22, "Grade": 45},
+        {"Name": "Vipul", "Age": 24, "Grade": 55}
+    ]
+    for student in school_dict:
+        print(f"name: {student['Name']}, Age: {student['Age']}, Grade - {student['Grade']}")
 
 
 if __name__ == "__main__":
